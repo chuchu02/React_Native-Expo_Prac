a5407f35d9658602f7b05e0afe818f8a7a70ea28
chuchu02 <rlaxogns73@gmail.com>
2022-10-06 10:15:09 +0900

    Dimensions를 이용하여 Device의 width, heigth 구하기

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Dimensions } from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get("window");
 
 export default function App() {
   return (
